d1c9fe5391a97935d30aab51a5bdc1060377b3f0
Yago Toro molina <ytoro-mo@c1r9s3.42malaga.com>
2022-09-07 13:09:38 +0200

    No leaks Mandatory & cleaner lib

--- src/keyhook.c
+++ src/keyhook.c
@@ -6,47 +6,46 @@
 /*   By: ytoro-mo < ytoro-mo@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 16:15:06 by Yago_42           #+#    #+#             */
-/*   Updated: 2022/09/07 12:51:16 by ytoro-mo         ###   ########.fr       */
+/*   Updated: 2022/09/07 13:00:32 by ytoro-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void ft_keyhook(mlx_key_data_t keydata, t_map_data *map)
+void	ft_keyhook(mlx_key_data_t keydata, t_map_data *map)
 {
- 	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
-    ft_key_up(map);
-  if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
-    ft_key_down(map);
-  if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
+	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
+		ft_key_up(map);
+	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
+		ft_key_down(map);
+	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
 		ft_key_right(map);
-  if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
-    ft_key_left(map);
-  if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-    ft_key_escape(map);
-  if (keydata.key == MLX_KEY_W && keydata.action == MLX_REPEAT)
-    ft_key_up(map);
-  if (keydata.key == MLX_KEY_S && keydata.action == MLX_REPEAT)
-    ft_key_down(map);
-  if (keydata.key == MLX_KEY_D && keydata.action == MLX_REPEAT)
-    ft_key_right(map);
-  if (keydata.key == MLX_KEY_A && keydata.action == MLX_REPEAT)
-    ft_key_left(map);
-}
-  
-void  ft_key_escape(t_map_data *map)
-{
-  mlx_close_window(map->mlx);
+	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
+		ft_key_left(map);
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+		ft_key_escape(map);
+	if (keydata.key == MLX_KEY_W && keydata.action == MLX_REPEAT)
+		ft_key_up(map);
+	if (keydata.key == MLX_KEY_S && keydata.action == MLX_REPEAT)
+		ft_key_down(map);
+	if (keydata.key == MLX_KEY_D && keydata.action == MLX_REPEAT)
+		ft_key_right(map);
+	if (keydata.key == MLX_KEY_A && keydata.action == MLX_REPEAT)
+		ft_key_left(map);
 }
 
-void  ft_map_moves(t_map_data *map)
+void	ft_key_escape(t_map_data *map)
 {
-    char  *moves;
-
-    map->mov++;
-    mlx_delete_image(map->mlx, map->txt);
-    moves = ft_strjoin_3("MOVES:	", ft_itoa(map->mov));
-    map->txt = mlx_put_string(map->mlx, moves, 0, map->map_hth);
-    free (moves);
+	mlx_close_window(map->mlx);
 }
- 
+
+void	ft_map_moves(t_map_data *map)
+{
+	char	*moves;
+
+	map->mov++;
+	mlx_delete_image(map->mlx, map->txt);
+	moves = ft_strjoin_3("MOVES:	", ft_itoa(map->mov));
+	map->txt = mlx_put_string(map->mlx, moves, 0, map->map_hth);
+	free(moves);
+}
